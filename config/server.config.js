f6f6e32cf8817843d9a49e06224a8312ec77bea6
Sergio Calero Robles <parabola.calero@gmail.com>
2019-05-02 16:36:32 +0100

    Removed authentication for mongodb. Added request module

--- config/server.config.js
+++ config/server.config.js
@@ -2,7 +2,7 @@ const SERVER_CONFIG = {
 
   /*** SERVER, MONGODB, PYTHON ***/
   SERVER_PORT : 3000,
-  MONGODB_URL : 'mongodb://edomusAdmin:edomuspassword@localhost:27017/edomus',
+  MONGODB_URL : 'mongodb://localhost:27017/edomus',
   JWT_SECRET: 'long-live-the-pisense',
   TOKEN_EXPIRE_TIME: 1296000, // 86400 seconds equivalent to 24 hours
   MONGODB_OPTIONS: {
