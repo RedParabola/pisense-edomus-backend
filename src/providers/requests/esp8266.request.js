f909c9d219f7cad5645a52a1c7ffb6c50e3a4de3
RedParabola <parabola.calero@gmail.com>
2019-05-02 18:01:03 +0200

    shit fix 3

--- src/providers/requests/esp8266.request.js
+++ src/providers/requests/esp8266.request.js
@@ -7,9 +7,9 @@ const request = require('request');
 function _request() {
 
   request.get('http://192.168.4.15/temp')
-  .on('response', function(response) {
-    console.log(response.success) // 200
-    console.log(response.Temperature) // 200
+  .on('response', function(response, body) {
+    console.log(body.success) // 200
+    console.log(body.Temperature) // 200
   })
   .on('error', function(response) {
     console.log('ERROR!') // 500
