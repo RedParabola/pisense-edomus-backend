435cdfdd6c51836845688f3671b4b2c1f4923520
RedParabola <parabola.calero@gmail.com>
2019-07-31 00:11:20 +0200

    Added shell execution through shelljs npm package. Now can execute asynchronously with _execAsync

--- src/providers/scripts/script.service.js
+++ src/providers/scripts/script.service.js
@@ -1,11 +1,23 @@
+var shell = require('shelljs');
+
 const composeBoardScripts = function (boardInfo, callback) {
-  // TODO
-  let err;
-  if (false) {
-    err = 'Mock composeBoardScripts FAILURE';
-  }
-  const scriptPathRef = 'script.path.ref';
-  callback(err, scriptPathRef);
+  let err, scriptPathRef;
+  _execAsync('bash sleep_example.sh', function(data) {
+    if (data === 'Completed') {
+      console.log('execution of bash sleep_example.sh ended');
+      scriptPathRef = 'script.path.ref';
+      callback(err, scriptPathRef);
+    } else if (data === 'Error') {
+      err = 'FAILURE composeBoardScripts';
+      callback(err);
+    }
+  }, function(error) {
+    console.log('Failed execution of bash sleep_example.sh');
+    if (error === 'Error') {
+      err = 'FAILURE composeBoardScripts';
+      callback(err);
+    }
+  });
 }
 
 // Call to flash the scripts into the usb-connected board
@@ -17,6 +29,16 @@ const flashBoardScripts = function (scriptRef, callback) {
   const success = true;
   callback(err, success);
 };
+
+function _execAsync(command, dataCallback, errorCallback) {
+  const child = shell.exec(command, {async:true});
+  child.stdout.on('data', function(data) {
+    dataCallback(data.replace(/(\r\n|\n|\r)/gm,""));
+  });
+  child.stderr.on('data', function(error) {
+    errorCallback(error.replace(/(\r\n|\n|\r)/gm,""));
+  });
+}
 
 const scriptService = {
   composeBoardScripts,
